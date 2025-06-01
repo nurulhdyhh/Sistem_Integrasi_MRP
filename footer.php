e11587f1c4fc1705a7385a4556c56f430ff03017
nurulhdyhh <nurhidyh950@gmail.com>
2025-06-01 11:13:34 +0700

    Second Commit

--- footer.php
+++ footer.php
@@ -1,116 +1,84 @@
 <style>
-        .list-unstyled li a {
-            color: white;
-            text-decoration: none;
-        }
+    .list-unstyled li a {
+        color: white;
+        text-decoration: none;
+    }
 
-        .list-unstyled li a:hover {
-            color: rgb(163, 211, 255);
-            text-decoration: none;
-        }
-        input.btn.i {
-            border: 2px solid white;
-            width: 75%;
-            padding: 7px;
-            border-top-right-radius: 0;
-            border-bottom-right-radius: 0;
-        }
+    .list-unstyled li a:hover {
+        color: rgb(163, 211, 255);
+        text-decoration: none;
+    }
 
-        button.btn.o {
-            border: 2px solid white;
-            padding: 7px;
-            font-weight: bold;
-            border-top-left-radius: 0;
-            border-bottom-left-radius: 0;
-        }
+    input.btn.i {
+        border: 2px solid white;
+        width: 75%;
+        padding: 7px;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+    }
 
-        ::placeholder {
-            color: white;
-        }
+    button.btn.o {
+        border: 2px solid white;
+        padding: 7px;
+        font-weight: bold;
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+    }
 
-        footer {
-            left: 0;
-            bottom: 0;
-            width: 100%;
-        }
-    </style>
-    <footer>
-        <div class="page-footer font-small indigo  bg-primary mt-3">
-            <div class="container-fluid text-center text-md-left">
-                <div class="row text-white">
-                    <div class="col-md-2 mx-auto">
-                        <h5 class="font-weight-bold text-uppercase mt-3 mb-2">ABOUT US</h5>
+    ::placeholder {
+        color: white;
+    }
+
+    footer {
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        /* Bisa tambahkan background dan padding */
+    }
+
+    .social-icons img {
+        margin-right: 8px;
+    }
+</style>
+
+<footer>
+    <div class="page-footer font-small indigo bg-primary mt-3 py-4 text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 mb-4">
+                    <h5 class="font-weight-bold text-uppercase mb-3">About Us</h5>
+                    <p>
                         Packindo menyediakan solusi kemasan inovatif dan berkualitas untuk industri makanan, minuman, kosmetik, dan farmasi.
-                        <br>
-                        <img src="assets/img/icon/ig.PNG" height="25px" width="25px" alt="">
-                        <img src="assets/img/icon/fb.png" height="25px" width="25px" alt="">
-                        <img src="assets/img/icon/twitter.png" height="23px" width="23px" alt="">
+                    </p>
+                    <div class="social-icons">
+                        <a href="#"><img src="assets/img/icon/ig.PNG" height="25" width="25" alt="Instagram"></a>
+                        <a href="#"><img src="assets/img/icon/fb.png" height="25" width="25" alt="Facebook"></a>
+                        <a href="#"><img src="assets/img/icon/twitter.png" height="25" width="25" alt="Twitter"></a>
                     </div>
-                    <div class="col-md-2.5 mx-auto">
-                        <h5 class="font-weight-bold text-uppercase mt-3 mb-2">CUSTOMER SERVICE</h5>
-                        <ul class="list-unstyled">
-                            <li>
-                                <a href="#!">Contact Us</a>
-                            </li>
-                            <li>
-                                <a href="#!">Ordering & Payment</a>
-                            </li>
-                            <li>
-                                <a href="#!">Delivery</a>
-                            </li>
+                </div>
 
-                        </ul>
-                    </div>
-                    <hr class="clearfix w-100 d-md-none">
-                    <div class="col-md-1.5 mx-auto">
-                        <h5 class="font-weight-bold text-uppercase mt-3 mb-2">CONTACT US</h5>
-                        <ul class="list-unstyled">
-                            +62 8167 2837 2626 <br>
-                            Packindo@gmail.com <br>
-                            Perum IKIP Gunung Anyar Indah Blok A-21
-                        </ul>
-                    </div>
-                    <hr class="clearfix w-100 d-md-none">
-                    <div class="col-md-2 mx-auto">
-        
-                   
+                <div class="col-md-3 mb-4">
+                    <h5 class="font-weight-bold text-uppercase mb-3">Customer Service</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="#!">Contact Us</a></li>
+                        <li><a href="#!">Ordering & Payment</a></li>
+                        <li><a href="#!">Delivery</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-3 mb-4">
+                    <h5 class="font-weight-bold text-uppercase mb-3">Contact Us</h5>
+                    <address>
+                        <strong>Phone:</strong> +62 8167 2837 2626<br>
+                        <strong>Email:</strong> <a href="mailto:Packindo@gmail.com" class="text-white">Packindo@gmail.com</a><br>
+                        <strong>Address:</strong> Perum IKIP Gunung Anyar Indah Blok A-21
+                    </address>
+                </div>
+
+                <div class="col-md-3 mb-4">
+                    <!-- Bisa isi dengan konten tambahan jika perlu, atau dihilangkan -->
                 </div>
             </div>
         </div>
-    </footer>
-
-    </body>
-    <!-- Js Dasar -->
-    <script src="assets/jquery/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <!-- Popper -->
-    <script src="assets/js/popper/popper.min.js"></script>
-    <!-- Owl Carausel -->
-    <script src="assets/js/owl/owl.carousel.js"></script>
-    <!-- Sweetalert -->
-    <script src="assets/js/sweetalert/sweetalert.min.js"></script>
-
-    <!-- Stok Detail Produk -->
-    <!-- Plugins js -->
-    <script src="plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
-    <script src="plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
-    <script src="plugins/bootstrap-maxlength/bootstrap-maxlength.js"></script>
-    <script src="plugins/bootstrap-touchspin/js/jquery.bootstrap-touchspin.min.js"></script>
-
-    <!-- Plugins Init js -->
-    <script src="admin/assets/pages/form-advanced.js"></script>
-
-    <!-- Datatable js -->
-    <script src="plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="plugins/datatables/dataTables.bootstrap4.min.js"></script>
-    <!-- Responsive examples -->
-    <script src="plugins/datatables/dataTables.responsive.min.js"></script>
-    <script src="plugins/datatables/responsive.bootstrap4.min.js"></script>
-    <script>
-        $(document).ready(function () {
-            $('#datatable').DataTable();
-        });
-    </script>
-
-    </html>
+    </div>
+</footer>
--- header.php
+++ header.php
@@ -1,183 +1,98 @@
 <?php 
     session_start();
     require "koneksi.php";
- ?>
+?>
 <!DOCTYPE html>
-<html lang="en" ng-app="myApp">
-
+<html lang="id">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <!-- Font -->
-    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0/css/font-awesome.min.css">
-    <!-- Owl carausel -->
-    <link rel="stylesheet" href="assets/js/owl/owl.carousel.css">
-    <link rel="stylesheet" href="assets/js/owl/owl.theme.css">
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="assets/img/icon/logo.png">
-    <link href="assets/css/material-design/css/materialdesignicons.css" rel="stylesheet" type="text/css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
-    <script src="assets/jquery/jquery.min.js"></script>
-    <link rel="stylesheet" href="plugins/owlcarousel/owl.carousel.min.js">
-    <link rel="stylesheet" href="plugins/owlcarousel/assets/owl.theme.default.min.css">
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135974525-2"></script>
-
-    <link href="plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-    <link href="plugins/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-
-<style>
-    body {
-        background-color: rgb(246, 245, 245);
-    }
-
-    .navbar {
-        padding: 0px;
-        margin: 0px;
-        box-shadow: 0px 2px 5px grey;
-    }
-
-    .navbar-light .navbar-nav .nav-link {
-        color: black;
-        font-size: 15px;
-        padding: 12px 20px;
-        position: relative;
-        transition: all 0.3s ease;
-    }
-
-    .navbar-light .navbar-nav .nav-link:hover {
-        color: #0066FF;
-        font-weight: 600;
-    }
-
-    .navbar-light .navbar-nav .nav-link::after {
-        content: '';
-        display: block;
-        width: 0;
-        height: 2px;
-        background: #0066FF;
-        transition: width 0.3s;
-        position: absolute;
-        bottom: 6px;
-        left: 20%;
-    }
-
-    .navbar-light .navbar-nav .nav-link:hover::after {
-        width: 60%;
-    }
-
-    .navbar .navbar-brand img {
-        width: 80px;
-        height: auto;
-    }
-
-    .navbar-btn {
-        padding: 5px 15px;
-        font-size: 14px;
-    }
-
-    /* Responsive adjustments */
-    @media(max-width:1200px) {
-        .navbar-nav li {
-            font-size: 15px;
-        }
-    }
-
-    @media(max-width:992px) {
-        .navbar-toggler {
-            margin: 10px 0px 10px 30px;
-            border-color: #ff4f81;
-        }
-
-        .navbar-toggler:hover,
-        .navbar-toggler:focus {
-            background-color: white;
-            border-color: #ff4f81;
-        }
-
-        .navbar-light {
-            background-color: white;
-        }
-
-        .navbar-nav .nav-link {
-            text-align: center;
-            background-color: white;
-        }
-
-        .navbar-nav .nav-link:hover {
-            background-color: #0066FF;
-            color: white;
-        }
-
-        .navbar-nav .nav-link::after {
-            display: none; /* hilangkan efek garis bawah di mobile */
-        }
-    }
-</style>
-
-
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Packindo</title>
+
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
+    <!-- Material Design Icons -->
+    <link href="assets/css/material-design/css/materialdesignicons.css" rel="stylesheet" />
+
+    <!-- SweetAlert & jQuery -->
+    <script src="assets/jquery/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+    <link rel="shortcut icon" href="assets/img/icon/logo.png" />
 </head>
+<body class="bg-light">
 
-<body>
-    <header>
-        <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white m-0 p-0">
-            <div class="container pl-3 pr-3">
-                <a class="navbar-brand" href="#">
-                    <img src="assets/img/icon/logo.png" width="100" height="80" alt="">
-                </a>
-
-                <?php if (isset($_SESSION["pelanggan"])): ?>
-                    <span class="mr-3 text-dark">
-                        Selamat datang, <strong><?php echo $_SESSION["pelanggan"]["username"]; ?></strong>
-                    </span>
-                <?php endif; ?>
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php">Produk</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="tambah-kategori.php">Kategori Produk</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="shop.php">Shop</a>
-                        </li>
-                        <li class="nav-item">
-                            <?php if (isset($_SESSION["pelanggan"])) {
-                                echo "<a class='nav-link' href='orderan.php'>Orderan</a>";
-                            } ?>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="profile.php">My Profile</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link position-relative d-inline-block" href="cart.php" style="padding: 0 10px;">
-                                <i class="mdi mdi-cart-outline text-primary" style="font-size: 20px;"></i>
-                                <span id="cart-count" class="badge badge-danger position-absolute" style="top: -8px; right: -10px; font-size: 10px;">
-                                    <?php echo isset($_SESSION['cart']) ? array_sum($_SESSION['cart']) : 0; ?>
-                                </span>
-                            </a>
-
-                        </li>
-                    </ul>
+<div class="container-fluid">
+    <div class="row">
+        <!-- Sidebar -->
+        <nav class="col-md-3 col-lg-2 d-md-block bg-white sidebar border-end min-vh-100 py-3">
+            <div class="position-sticky px-3">
+                <div class="text-center border-bottom pb-3 mb-4">
+                    <a href="index.php" class="d-inline-block">
+                        <img src="assets/img/icon/logo.png" alt="Logo" height="170" />
+                    </a>
                 </div>
 
-                <?php if (isset($_SESSION["pelanggan"])) : ?>
-                    <button class="btn btn-primary navbar-btn m-2" onclick="window.location.href='logout.php'">Logout</button>
-                <?php else: ?>
-                    <button class="btn btn-primary navbar-btn m-2" onclick="window.location.href='login.php'">Login</button>
-                <?php endif ?>
+                <?php if (isset($_SESSION["pelanggan"])): ?>
+                    <div class="px-2 py-2 fs-5 fw-semibold mb-4 text-dark">
+                    Selamat datang,<br />
+                        <?php echo $_SESSION["pelanggan"]["username"]; ?>
+                    </div>
+                <?php endif; ?>
+
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-3">
+                        <a class="nav-link text-dark" href="index.php">
+                            <i class="fas fa-box me-2"></i> Produk
+                        </a>
+                    </li>
+                    <li class="nav-item mb-3">
+                        <a class="nav-link text-dark" href="tambah-kategori.php">
+                            <i class="fas fa-list me-2"></i> Kategori Produk
+                        </a>
+                    </li>
+                    <li class="nav-item mb-3">
+                        <a class="nav-link text-dark" href="shop.php">
+                            <i class="fas fa-store me-2"></i> Shop
+                        </a>
+                    </li>
+                    <?php if (isset($_SESSION["pelanggan"])): ?>
+                        <li class="nav-item mb-3">
+                            <a class="nav-link text-dark" href="orderan.php">
+                                <i class="fas fa-clipboard-list me-2"></i> Orderan
+                            </a>
+                        </li>
+                    <?php endif; ?>
+                    <li class="nav-item mb-3">
+                        <a class="nav-link text-dark" href="profile.php">
+                            <i class="fas fa-user me-2"></i> My Profile
+                        </a>
+                    </li>
+                    <li class="nav-item mb-3">
+                        <a class="nav-link d-flex justify-content-between align-items-center text-dark" href="cart.php">
+                            <span><i class="mdi mdi-cart-outline text-primary me-2"></i> Keranjang</span>
+                            <span class="badge bg-danger rounded-pill">
+                                <?php echo isset($_SESSION['cart']) ? array_sum($_SESSION['cart']) : 0; ?>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item mb-3">
+                        <?php if (isset($_SESSION["pelanggan"])) : ?>
+                            <a class="nav-link text-danger" href="logout.php">
+                                <i class="mdi mdi-logout me-2"></i> Logout
+                            </a>
+                        <?php else: ?>
+                            <a class="nav-link text-primary" href="login.php">
+                                <i class="mdi mdi-login me-2"></i> Login
+                            </a>
+                        <?php endif ?>
+                    </li>
+                </ul>
             </div>
         </nav>
-    </header>
+
+        <!-- Main content -->
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
+            <!-- Konten halaman dimulai di sini -->
